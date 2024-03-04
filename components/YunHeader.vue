bb8105272d844355dca758230459013203b17e6f
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-05 01:48:30 +0800

    看板娘提交

--- components/YunHeader.vue
+++ components/YunHeader.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
+import { useScriptTag } from '@vueuse/core'
 import YunHeader from 'valaxy-theme-yun/components/YunHeader.vue'
+useScriptTag("https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js")
 </script>
 
 
