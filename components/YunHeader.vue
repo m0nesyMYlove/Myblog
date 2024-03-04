cdcc80753842a068e20c94d23bc3d97ff1fe2490
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-05 01:37:50 +0800

    看板娘添加

---
+++ components/YunHeader.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import YunHeader from 'valaxy-theme-yun/components/YunHeader.vue'
+</script>
+
+<template>
+  <YunHeader>
+
+    <script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>
+
+  </YunHeader>
+</template>
