5c54dca5f218c7f2484fd6061ffa09f5102a3153
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-05 02:02:21 +0800

    看板娘提交

--- components/YunHeader.vue
+++ components/YunHeader.vue
@@ -1,13 +1,7 @@
 <script lang="ts" setup>
 import YunHeader from 'valaxy-theme-yun/components/YunHeader.vue'
-
-// 创建一个新的 <script> 元素，用于加载指定的脚本
-const scriptElement = document.createElement('script');
-scriptElement.src = 'https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js';
-
-// 将新的 <script> 元素插入到原网页头文件中
-document.head.appendChild(scriptElement);
+useScriptTag('https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js')
 </script>
 <YunHeader>
-    <!-- 保留原有的 YunHeader 内容 -->
+    
 </YunHeader>
