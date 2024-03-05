a8d92e73fcb3f012b2f534ac90a3ff88b06491ab
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-05 13:54:55 +0800

    看板娘提交

--- components/VueFooter.vue
+++ components/VueFooter.vue
@@ -4,14 +4,7 @@ import YunFooter from "valaxy-theme-yun/components/YunFooter.vue";
 
 <template>
   <YunFooter>
-    <VCLiveTime start-time="2021-09-20">
-      <template #live-time-before>
-        <span>已建站</span>
-      </template>
-      <template #live-time-after>
-      </template>
-    </VCLiveTime>
-    <script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>
+    useScriptTag("https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js")
   </YunFooter>
 </template>
 
