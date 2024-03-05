ac812ef62de33524ed782ea9ff915a801b5684e7
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-05 14:01:36 +0800

    看板娘提交

--- components/VueFooter.vue
+++ components/VueFooter.vue
@@ -5,6 +5,11 @@ import YunFooter from "valaxy-theme-yun/components/YunFooter.vue";
 <template>
   <YunFooter>
     useScriptTag("https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js")
+    <div>
+      <a href="https://icp.gov.moe/?keyword=20230105" target="_blank"
+        >萌 ICP 备 20240305 号</a
+      >
+    </div>
   </YunFooter>
 </template>
 
