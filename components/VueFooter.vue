4e207a1931c4b88676a2d5632f382c3dbc38cdb4
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-05 14:08:22 +0800

    看板娘

--- components/VueFooter.vue
+++ components/VueFooter.vue
@@ -6,11 +6,6 @@ import { useScriptTag } from "valaxy";
 <template>
   <YunFooter>
     useScriptTag("https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js")
-    <div>
-      <a href="https://icp.gov.moe/?keyword=20230105" target="_blank"
-        >萌 ICP 备 20240305 号</a
-      >
-    </div>
   </YunFooter>
 </template>
 
