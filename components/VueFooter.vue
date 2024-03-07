cc1d3ebf1f3ea3de5424998f58d94633b4ff4b98
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-07 15:24:45 +0800

    回退

--- components/VueFooter.vue
+++ components/VueFooter.vue
@@ -7,8 +7,6 @@
 
 <script lang="ts" setup>
 import YunFooter from "valaxy-theme-yun/components/YunFooter.vue";
-import {useScriptTag} from '@vueuse/core';
-useScriptTag("https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js");
 </script>
 
 <style>
