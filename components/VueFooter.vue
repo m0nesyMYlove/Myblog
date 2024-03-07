2e58fbcdec497b200bef2e32e00fc4e806547a3e
m0nesyMYlove <148782075+m0nesyMYlove@users.noreply.github.com>
2024-03-07 14:53:51 +0800

    看板娘

--- components/VueFooter.vue
+++ components/VueFooter.vue
@@ -1,15 +1,16 @@
-<template>
-  <YunFooter>
-  <div>
-  </div>
-  </YunFooter>
-</template>
-
 <script lang="ts" setup>
 import YunFooter from "valaxy-theme-yun/components/YunFooter.vue";
 import {useScriptTag} from '@vueuse/core'
 useScriptTag("https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js")
 </script>
+
+<template>
+  <YunFooter>
+  <div>
+    <script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>
+  </div>
+  </YunFooter>
+</template>
 
 <style>
 @media (max-width: 1000px) {
